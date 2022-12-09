145c064af6e026801e699d9a3f4aeb28c80503e2
Daliborka Vidic Gutiérrez <daliborka_vg@hotmail.com>
2022-12-09 16:09:11 +0000

    Finiiiiitoooo2

--- src/main/java/es/cifpm/VidicGutierrezDaliborkaFarmacias.java
+++ src/main/java/es/cifpm/VidicGutierrezDaliborkaFarmacias.java
@@ -1,12 +1,5 @@
 package es.cifpm;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
